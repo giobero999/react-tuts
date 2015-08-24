bf32b9076b444423248ac2d43fc4c9db0a533735
giobero999 <giobero999@gmail.com>
2015-08-24 16:30:16 +0400

    components, lists

---
+++ app/index.h
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>react tuts</title>
+</head>
+<body>
+	<div id="content"></div>
+</body>
+<script src="https://fb.me/react-0.13.3.js"></script>
+<script src="https://fb.me/JSXTransformer-0.13.3.js"></script>
+<div id="content"></div>
+    <script type="text/jsx">
+      React.render(
+        <h1>Hello, world!</h1>,
+        document.getElementById('content')
+      );
+    </script>
+</html>
